30058e9e982992da0f71b0a726e9bffd6ac784cc
k2ev <30880794+k2ev@users.noreply.github.com>
2019-04-22 00:44:56 -0400

    first commit before directives
    Signed-off-by: k2ev <30880794+k2ev@users.noreply.github.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'second-app';
+  featureLoaded = '';
+
+  onFeatureLoaded(feature: string) {
+    this.featureLoaded = feature;
+  }
 }
+
